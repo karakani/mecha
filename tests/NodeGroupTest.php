0c1666941d8aa6ab0f4b32ecfd28edd5861817a5
T. Karakane <toshihiro.karakane@gmail.com>
2020-06-01 21:29:57 +0900

    add test cases

--- tests/NodeGroupTest.php
+++ tests/NodeGroupTest.php
@@ -17,4 +17,36 @@ class NodeGroupTest extends TestCase
 
         $this->assertEquals(10, $count);
     }
+
+    public function testArrayAccess()
+    {
+        $instance = new NodeGroup(range(0, 9));
+
+        $this->assertTrue(isset($instance[9]));
+        $this->assertFalse(isset($instance[10]));
+    }
+
+    public function testOverrideItems()
+    {
+        $instance = new NodeGroup(range(0, 9));
+
+        $this->expectException(\Exception::class);
+        $instance[5] = -1; // 更新できず例外が投げられる
+    }
+
+    public function testOverrideItem2()
+    {
+        $instance = new NodeGroup(range(0, 9));
+
+        $this->expectException(\Exception::class);
+        $instance[] = 1; // 更新できず例外が投げられる
+    }
+
+    public function testDeleteItems()
+    {
+        $instance = new NodeGroup(range(0, 9));
+
+        $this->expectException(\Exception::class);
+        unset($instance[9]);
+    }
 }
