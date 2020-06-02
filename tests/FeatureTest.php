7d0863e679cd5da40327531041e98258286195e8
T. Karakane <toshihiro.karakane@gmail.com>
2020-06-02 11:22:19 +0900

    Featureのカラム数のチェックを外す場合のテストケースを追加

--- tests/FeatureTest.php
+++ tests/FeatureTest.php
@@ -38,13 +38,36 @@ class FeatureTest extends TestCase
         $this->assertEquals(['追加1', '追加2'], $feature->additionalParams);
     }
 
+    public function testContainsLatinWord()
+    {
+        $feature = Feature::fromCsv('名詞,固有名詞,組織,*,*,*,*');
+
+        $this->assertEquals('名詞', $feature->pos);
+        $this->assertEquals('固有名詞', $feature->pos_sub1);
+        $this->assertEquals('組織', $feature->pos_sub2);
+        $this->assertNull($feature->pos_sub3);
+        $this->assertNull($feature->conjugation_form);
+        $this->assertNull($feature->conjugation);
+        $this->assertNull($feature->lexical);
+        $this->assertNull($feature->yomi);
+        $this->assertNull($feature->pronunciation);
+        $this->assertNull($feature->additionalParams);
+    }
+
+    /**
+     * 通常とは異なる形式のデータが存在する場合の取り扱いのテスト
+     *
+     * Featureはカラム位置のみによりデータの設定を行う。
+     *
+     */
     public function testUnexpected()
     {
         $input = 'one,two';
 
         $feature = Feature::fromCsv($input);
 
-        $this->assertNull($feature->pos);
+        $this->assertEquals('one', $feature->pos);
+        $this->assertEquals('two', $feature->pos_sub1);
         $this->assertEquals($input, $feature->raw);
     }
 
